7acf9833a35d3ca18cc4c92faa4286225cc96bfe
Amanda Walter <anhart@gmail.com>
2015-02-12 07:32:02 -0700

    Remove gravatar from users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,5 @@
 module UsersHelper
 
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 100 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "gravatar")
-  end
-
   def liked_artists(user)
     artists = []
     Work.all.each do |work|
